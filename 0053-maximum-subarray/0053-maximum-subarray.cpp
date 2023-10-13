c0b1e2db1b38b3e21ea14e4dba2177cb2cb2af87
Anurag Bhati <110467827+anurag8602@users.noreply.github.com>
2023-10-13 22:42:49 +0530

    Time: 57 ms (99.44%), Space: 68 MB (43.3%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,16 @@
+#define fast ({ios_base::sync_with_stdio(0);\
+         cin.tie(0); \
+         cout.tie(0);})
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        int i=0,j=0;
-        int ans=0,s=0;
-        int maxi=INT_MIN;
+        fast;
+        int j=0,s=0,maxi=INT_MIN;
         for(;j<nums.size();++j)
         {
             s+=nums[j];
             maxi=max(maxi,s);
-            if(s<0) s=0;
+            s=max(s,0);
         }
         return maxi;
     }
