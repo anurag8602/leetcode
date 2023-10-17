051c3e71c68785cfdaa164b79df0469cd04c0642
Anurag Bhati <110467827+anurag8602@users.noreply.github.com>
2023-10-18 01:53:23 +0530

    Time: 3 ms (48.78%), Space: 6.8 MB (49.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,8 @@
+int su = []{std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        su;
         int c=0;
         int n=s.length()-1;
         while(s[n]==' ')
