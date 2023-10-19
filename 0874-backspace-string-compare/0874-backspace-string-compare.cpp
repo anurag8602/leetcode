50cea13255447edde7d81aaff521a6aee8a1161e
Anurag Bhati <110467827+anurag8602@users.noreply.github.com>
2023-10-20 00:05:58 +0530

    Time: 3 ms (47.11%), Space: 7 MB (7.09%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -3,23 +3,18 @@ public:
     bool backspaceCompare(string s, string t) {
         
         stack<char> s1, s2;
-        // check elements of s
         for(int i=0; i<s.size(); i++){
             if(s[i]!='#')
             s1.push(s[i]);
             else if(!s1.empty())
             s1.pop();
         }
-
-        // check elements of t
         for(int i=0; i<t.size(); i++){
             if(t[i]!='#')
             s2.push(t[i]);
             else if(!s2.empty())
             s2.pop();
         }
-
-
         return s1==s2;
     }
 };
