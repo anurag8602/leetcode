fa9c1809401e92ae093470bfd22678b603f50b88
Anurag Bhati <110467827+anurag8602@users.noreply.github.com>
2023-10-24 12:44:47 +0530

    Time: 12 ms (23.46%), Space: 12.7 MB (7.49%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<string> ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
         vector<string>  tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         vector<string>  hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
