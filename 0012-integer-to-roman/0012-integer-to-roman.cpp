55651ce26fd6b4a79360c514c153077f6f718d8f
Anurag Bhati <110467827+anurag8602@users.noreply.github.com>
2023-10-24 12:46:55 +0530

    Time: 20 ms (6.81%), Space: 12.5 MB (8.27%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> v1={"M","CM","D","CD","C","XC","L","XL",
-                           "X","IX","V","IV","I"};
-        vector<int> v2={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string res="";
-        for(int i=0; num>0; i++){
-            while(num>=v2[i]){
-                res=res+v1[i];
-                num=num-v2[i];
-            }
-        }
-        return res;
+        vector<string> ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        vector<string>  tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        vector<string>  hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        vector<string>  ths={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
