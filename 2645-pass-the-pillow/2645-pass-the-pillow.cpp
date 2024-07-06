d67c4c39666ace9ba508359651d6204ab6e569c2
Anurag Bhati <anuragbhatioff@gmail.com>
2024-07-06 22:18:20 +0530

    Time: 3 ms (16.54%), Space: 6.9 MB (96.49%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if(time<n) return time+1;
+        int x=time/(n-1);
+        if(x%2==0)
+            return 1+time%(n-1);
+        return n-time%(n-1);
+    }
+};
