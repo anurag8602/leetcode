c0b2c8eb76f7b20f5a88a95470cd63e662d0d288
Anurag Bhati <anuragbhatioff@gmail.com>
2024-07-06 22:23:26 +0530

    Time: 2 ms (54.14%), Space: 7.3 MB (30.58%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(time<n) return time+1;
         int x=time/(n-1);
         if(x%2==0)
